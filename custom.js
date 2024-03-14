6dbb23abca4185b14da87873b6c09bc4a692ba21
aporve <aporvep@gmail.com>
2024-03-14 11:23:01 +0530

    Changed Code

--- custom.js
+++ custom.js
@@ -1,8 +1,21 @@
+(function injectJS() {
+  try {
+    var iFrameHead = window.frames["ymIframe"].document.getElementsByTagName("head")[0];
+    var modularBars = document.createElement('script');
+    modularBars.type = 'text/javascript';
+    // modularBars.src = 'https://app.yellowmessenger.com/components/public/webviews/gskPortugal/assets/js/childIframe_pt.js';
+    iFrameHead.appendChild(modularBars);
+    injectDynamicCssToChild();
+  } catch (e) {
+    console.error("failed while inserting to iFrame", e);
+  }
+})();
+
 function injectDynamicCssToChild() {
   var ymFrameHead = window.frames["ymIframe"].document.getElementsByTagName("head")[0];
   var modularStyles = document.createElement('style');
   modularStyles.type = 'text/css';
-  var css = '.feedback-card-doc { display: none}';
+  var css = '.feedback-card-doc { display: none !important, }';
   if (modularStyles.styleSheet) {
     modularStyles.styleSheet.cssText = css;
   } else {
